52f10be4a577e8c5a1f4157fd6cee11c16fd5699
Your Name <you@example.com>
2017-09-08 11:50:39 +0530

    add files

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { CoursesComponent } from './courses.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
